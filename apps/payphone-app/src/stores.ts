2e54d6476d3dd70e1494173be308111e4a912be3
Ncookiez <ncukiert@gmail.com>
2023-11-25 19:42:25 -0500

    Added app install checks

--- apps/payphone-app/src/stores.ts
+++ apps/payphone-app/src/stores.ts
@@ -3,8 +3,9 @@ import { updateWalletBalance } from './utils'
 import { writable } from 'svelte/store'
 import { type Address } from 'viem'
 
-export const isAppInstalled = writable<boolean>(false)
 export const appView = writable<AppView>(AppView.loading)
+
+export const isAppInstalled = writable<boolean>(false)
 
 export const walletAddress = writable<Address | undefined>()
 export const walletBalance = writable<bigint | undefined>()
