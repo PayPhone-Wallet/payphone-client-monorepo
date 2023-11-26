f2f1fe7fdb1766dbed7100fbd0fac3d143e4b2d8
Ncookiez <ncukiert@gmail.com>
2023-11-25 20:46:01 -0500

    Added basic setup steps

---
+++ apps/payphone-app/src/lib/SetupStepListItem.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  export let stepId: number
+  export let currentStepId: number
+  export let text: string
+
+  $: isActive = currentStepId >= stepId
+  $: isDone = currentStepId > stepId
+</script>
+
+<li class:active={isActive}>
+  <span>
+    {text}
+    <i class="icofont-check" class:done={isDone} />
+  </span>
+</li>
+
+<style>
+  li {
+    color: var(--green-400);
+  }
+
+  li.active {
+    color: var(--green-600);
+    font-weight: 700;
+  }
+
+  li > span {
+    display: flex;
+    gap: 0.5em;
+  }
+
+  i.icofont-check {
+    font-size: 1.5em;
+    opacity: 0;
+  }
+
+  i.icofont-check.done {
+    opacity: 1;
+  }
+</style>
--- apps/payphone-app/src/views/SetupView.svelte
+++ apps/payphone-app/src/views/SetupView.svelte
@@ -1,14 +1,45 @@
 <script lang="ts">
+  import SetupStepListItem from '../lib/SetupStepListItem.svelte'
+
+  let currentStepId = 1
+
+  const setupSteps: string[] = ['Install PayPhone', 'Create wallet', 'Pair with your device']
 </script>
 
-<!-- TODO: this view should make sure the app is installed and a wallet address is generated -->
-
 <section id="setup-view">
-  <span>we do be settin' up a wallet</span>
+  <h1>Set up your digital wallet</h1>
+  <ol class="setup-steps">
+    {#each setupSteps as step, i}
+      <SetupStepListItem stepId={i} {currentStepId} text={step} />
+    {/each}
+  </ol>
+  <div class="setup-step-content">
+    {#if currentStepId === 0}
+      <span>step 1 content</span>
+    {:else if currentStepId === 1}
+      <span>step 2 content</span>
+    {:else if currentStepId === 2}
+      <span>step 3 content</span>
+    {/if}
+  </div>
 </section>
 
 <style>
   #setup-view {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
     height: 100%;
+    gap: 2.5em;
+  }
+
+  h1 {
+    font-size: 1.8em;
+  }
+
+  .setup-steps {
+    display: flex;
+    flex-direction: column;
+    text-align: start;
   }
 </style>
