72be5df4d8a1fab817743be264faa70627d20fc9
Ncookiez <ncukiert@gmail.com>
2023-11-26 00:46:05 -0500

    Setup step 2 placeholder functionality

--- apps/payphone-app/src/config.ts
+++ apps/payphone-app/src/config.ts
@@ -4,3 +4,14 @@ import { NETWORK } from '@payphone-client-monorepo/utilities'
  * App's Network
  */
 export const appChainId = NETWORK.base
+
+/**
+ * Possible Setup Drawing Prompts
+ */
+export const setupDrawingPrompts: string[] = [
+  'a smiley face',
+  'a banana',
+  'a tree',
+  'a train',
+  'a stick figure'
+]
--- apps/payphone-app/src/views/SetupView.svelte
+++ apps/payphone-app/src/views/SetupView.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
+  import { setupDrawingPrompts } from '../config'
   import SetupStepListItem from '../lib/SetupStepListItem.svelte'
   import { beforeAppInstallPromptEvent, isAppInstalled } from '../stores'
 
   const setupSteps: string[] = ['Install PayPhone', 'Create wallet', 'Pair with your device']
+  const drawingPrompt = setupDrawingPrompts[Math.floor(Math.random() * setupDrawingPrompts.length)]
   let currentStepId = 0
 
   $: isInstallButtonEnabled = !$isAppInstalled && !!$beforeAppInstallPromptEvent
@@ -13,15 +15,22 @@
       $beforeAppInstallPromptEvent.prompt()
     }
   }
+
+  const onDraw = () => {
+    // TODO: get drawing data, check if it is sufficient before moving on to next step
+    currentStepId++
+  }
 </script>
 
 <section id="setup-view">
-  <h1>Set up your digital wallet</h1>
-  <ol class="setup-steps">
-    {#each setupSteps as step, i}
-      <SetupStepListItem stepId={i} {currentStepId} text={step} />
-    {/each}
-  </ol>
+  <div class="setup-header">
+    <h1>Set up your digital wallet</h1>
+    <ol>
+      {#each setupSteps as step, i}
+        <SetupStepListItem stepId={i} {currentStepId} text={step} />
+      {/each}
+    </ol>
+  </div>
   <div class="setup-step-content">
     {#if currentStepId === 0}
       <button on:click={onClickInstallApp} disabled={!isInstallButtonEnabled}>
@@ -32,7 +41,16 @@
         install the app, check for an installation icon on the top right or try a different browser.
       </p>
     {:else if currentStepId === 1}
-      <span>step 2 content</span>
+      <h2 class="draw-instructions">Draw {drawingPrompt} to create your wallet</h2>
+      <!-- TODO: implement drawing functionality -->
+      <div id="draw-canvas" on:click={onDraw} />
+      <span class="draw-info">
+        Why {drawingPrompt}?
+        <span class="tooltip">
+          We use your drawing as part of the randomness necessary to generate a unique cryptographic
+          key for your wallet.
+        </span>
+      </span>
     {:else if currentStepId === 2}
       <span>step 3 content</span>
     {/if}
@@ -44,15 +62,23 @@
     display: flex;
     flex-direction: column;
     align-items: center;
+    justify-content: space-between;
     height: 100%;
     gap: 2.5em;
   }
 
-  h1 {
+  .setup-header {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 2.5em;
+  }
+
+  .setup-header > h1 {
     font-size: 1.8em;
   }
 
-  .setup-steps {
+  .setup-header > ol {
     display: flex;
     flex-direction: column;
     text-align: start;
@@ -62,7 +88,6 @@
     display: flex;
     flex-direction: column;
     align-items: center;
-    margin-top: auto;
   }
 
   .install-info {
@@ -73,5 +98,58 @@
 
   .install-info > strong {
     font-weight: 500;
+  }
+
+  .draw-instructions {
+    color: var(--green-700);
+    font-size: 1.5em;
+    font-weight: 600;
+  }
+
+  #draw-canvas {
+    width: min(40em, 80%);
+    aspect-ratio: 16/9;
+    background-color: var(--green-50);
+    border-radius: 0.5em;
+    margin: 0.75em 0;
+  }
+
+  .draw-info {
+    position: relative;
+    display: inline-block;
+    text-decoration: underline;
+  }
+
+  .tooltip {
+    --width: min(35em, 55vw);
+    --padding-x: 1em;
+    position: absolute;
+    bottom: 100%;
+    left: 50%;
+    visibility: hidden;
+    width: var(--width);
+    margin-bottom: 0.5em;
+    margin-left: calc(0em - var(--padding-x) - var(--width) / 2);
+    padding: 0.5em var(--padding-x);
+    color: var(--green-700);
+    background-color: var(--green-400);
+    border-radius: 0.4em;
+    font-size: 0.9em;
+  }
+
+  .tooltip::after {
+    --arrow-size: 0.5em;
+    content: '';
+    position: absolute;
+    top: 100%;
+    left: 50%;
+    margin-left: calc(0em - var(--arrow-size));
+    border-width: var(--arrow-size);
+    border-style: solid;
+    border-color: var(--green-400) transparent transparent transparent;
+  }
+
+  .draw-info:hover > .tooltip {
+    visibility: visible;
   }
 </style>
