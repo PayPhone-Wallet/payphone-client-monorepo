fdc1f640c20e3b6dfd2a5a85a0df3d6cb33db168
Ncookiez <ncukiert@gmail.com>
2023-11-26 22:40:05 -0500

    Clickable Icon

--- apps/payphone-app/src/lib/Icon.svelte
+++ apps/payphone-app/src/lib/Icon.svelte
@@ -1,11 +1,34 @@
 <script lang="ts">
+  import { appView } from '../stores'
+  import { AppView } from '../types'
   import icon from '/icon.svg'
+
+  $: isClickable = $appView === AppView.send || $appView === AppView.receive
+
+  const onClick = () => {
+    if (isClickable) {
+      appView.set(AppView.wallet)
+    }
+  }
 </script>
 
-<img src={icon} id="payphone-icon" alt="PayPhone Icon" />
+<div
+  id="payphone-icon"
+  on:click={onClick}
+  on:keydown={onClick}
+  role="button"
+  tabindex={1}
+  class:clickable={isClickable}
+>
+  <img src={icon} alt="PayPhone Icon" />
+</div>
 
 <style>
   #payphone-icon {
     height: 4em;
+  }
+
+  #payphone-icon.clickable {
+    cursor: pointer;
   }
 </style>
